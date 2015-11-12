68e6073b77a66156ee03107d85ee22b83cadfef1
NicoleAlves <nicole.alves30@gmail.com>
2015-11-12 14:19:49 -0200

    Reações em cadeia

---
+++ Assets/Resources/Scripts/Neutron_Behaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Neutron_Behaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Uranio")
+		{
+			col.gameObject.SendMessage("Split");
+			Destroy(this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector2(-1*Time.deltaTime,0));
+	}
+}
---
+++ Assets/Resources/Scripts/Uranio_Behaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uranio_Behaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Split()
+	{
+		Object[] n = Resources.LoadAll("Prefabs");
+		Quaternion rotation = Quaternion.identity;
+		float angle = Random.Range(0, 360F);
+		rotation.eulerAngles = new Vector3(0,0,angle);
+		Instantiate(n[0],new Vector3(this.transform.position.x + 0.5f,this.transform.position.y,10f),rotation);
+		Instantiate(n[1],new Vector3(this.transform.position.x - 0.5f,this.transform.position.y,10f),rotation);
+		for(int i = 0;i< 3;i++){
+		angle = Random.Range(0, 360F);
+		rotation.eulerAngles = new Vector3(0,0,angle);
+		Instantiate(n[2],transform.position,rotation);	
+		}
+		
+		Destroy(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
